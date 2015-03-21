d38814a13d23b934eec2bfe77d729cea93f5c00a
Huaming Rao <huaming.rao@gmail.com>
2015-03-22 00:35:22 +0800

    update a verion just for fixing the typos in README

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'huaming:jiathis-share',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'This is a plugin for adding jiathis share buttons to meteor app',
   // URL to the Git repository containing the source code for this package.
