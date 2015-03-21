5f1f679323b812b84290481c87c8a1efe3ebcc6e
Huaming Rao <huaming.rao@gmail.com>
2015-03-21 20:31:23 +0800

    change github repo name

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'This is a plugin for adding jiathis share buttons to meteor app',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://www.github.com/raohuaming/jiathis-share',
+  git: 'https://www.github.com/raohuaming/meteor-jiathis',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
